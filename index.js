77617e62c84f570ad4b077ad7a7b4c6033bc7148
levina <lewatron011@gmail.com>
2026-08-21 23:41:05 +0500

    update

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const express = require('express')
+const app = express()
+const http = require('http')
+const path = require('path')
+
+app.listen(3000, () => {
+    console.log('listen and repeat' + 3000)
+})
