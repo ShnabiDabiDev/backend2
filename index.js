28746a11a8ada44b70a3c843f06275382e45376f
levina <lewatron011@gmail.com>
2026-08-22 23:31:42 +0500

    Update 6

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get("/", (req, res) => {
   res.redirect("https://design-30f.pages.dev/");
 });
 
-app.post('backend2-production-046d.up.railway.app/api/profile/', (req, res) => {
+app.post('backend2-production-046d.up.railway.app/', (req, res) => {
     console.log(req.body)
     res.json({
         data: 'get'
