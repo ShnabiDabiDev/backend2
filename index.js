9c2e008c4bb3712ef66f2ad478a102b227f9045c
levina <lewatron011@gmail.com>
2026-08-22 23:13:03 +0500

    Update 4

--- index.js
+++ index.js
@@ -4,10 +4,19 @@ const app = express()
 const http = require('http')
 const path = require('path')
 
+app.use(express.json())
+
 app.listen(3000, () => {
     console.log('listen and repeat' + 3000)
 })
 
 app.get("/", (req, res) => {
-  res.redirect("https://design-30f.pages.dev/profile");
+  res.redirect("https://design-30f.pages.dev/");
 });
+
+app.post('backend2-production-046d.up.railway.app/api/profile/', (req, res) => {
+    console.log(req.body)
+    res.json({
+        data: 'get'
+    })
+})
