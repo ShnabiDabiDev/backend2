9dcb65dfd2a520be42d9e8923414294314eb5bbe
levina <lewatron011@gmail.com>
2026-08-24 20:56:03 +0500

    Update DATABASE

--- index.js
+++ index.js
@@ -35,4 +35,4 @@ app.post('/api/profile', (req, res) => {
   })
 })
 
-pg.query('SELECT * FROM users WHERE username = $1, passwordhash = $2' ['balyaka', '3232323'])
+await pg.query("INSERT INTO users (username, passwordhash) VALUES ($1, $2)", ['bablaka', '3134234'])
